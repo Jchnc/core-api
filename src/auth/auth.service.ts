8789915397ffadecc46c50e8a7fea448f52a2f85
Jean <jchrisnc@gmail.com>
2026-04-24 17:22:08 -0500

    feat(auth): change refresh cookie samesite policy to lax

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -450,7 +450,7 @@ export class AuthService {
     res.cookie(REFRESH_COOKIE_NAME, token, {
       httpOnly: true,
       secure: isProd,
-      sameSite: 'strict',
+      sameSite: 'lax',
       path: '/',
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in ms
     });
